8bebd544dbaa23aafdf7d115321c2b7b64b4c6e3
Zppix <rideahorse31@hotmail.com>
2016-01-26 14:23:07 -0600

    Added a new line

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <HTML>
 <body>
 <P> This is a Tutorial Website, Anyone can edit.</p>
+<p>Open up Welcome.html (will insert clickable link soon)</p>
 <P> Copyright 2016 Zppix-Ballee Franchise </P> ### Please do not remove this or modify this line. ###
